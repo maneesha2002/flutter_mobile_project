2836f1de67a90d5bc525ad943e00db4f81feca2d
Maneesha Kaushalya <148955760+maneesha2002@users.noreply.github.com>
2024-04-04 15:42:37 +0530

    home page design

--- LostSeeker/LostSeeker/lib/screens/home.dart
+++ LostSeeker/LostSeeker/lib/screens/home.dart
@@ -63,7 +63,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: const Text("Hom"),
         centerTitle: true,
         backgroundColor: Colors.blue[800],
       ),
