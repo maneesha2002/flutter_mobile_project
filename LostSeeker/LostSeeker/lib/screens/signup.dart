ec8366c0ff61ec2fc3ca69bd9703d8cc354af8fe
Maneesha Kaushalya <148955760+maneesha2002@users.noreply.github.com>
2024-04-07 21:04:54 +0530

    Update signup.dart

--- LostSeeker/LostSeeker/lib/screens/signup.dart
+++ LostSeeker/LostSeeker/lib/screens/signup.dart
@@ -23,7 +23,7 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 7, 224, 203),
+      backgroundColor: Color.fromARGB(255, 183, 245, 239),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 40),
